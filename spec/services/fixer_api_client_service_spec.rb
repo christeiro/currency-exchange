01b24645874a143674a69e48b1dbd6dcbe063df1
Krister Vals <krister.vals@gmail.com>
2016-09-01 18:10:33 +0300

    Fixer Api Client Service test with fixed date

--- spec/services/fixer_api_client_service_spec.rb
+++ spec/services/fixer_api_client_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe FixerApiClientService do
   it 'returns a list of currencies from fixer.io' do
     VCR.use_cassette "api_response_success" do
-      date = Time.new().strftime('%Y-%m-%d')
+      date = Time.new(2016, 9, 1).strftime('%Y-%m-%d')
       supported_currencies = 'USD,NOK,SEK'
       response = FixerApiClientService.new(date: date, symbols: supported_currencies).perform
       expect(response).to be_an_instance_of(Hash)
